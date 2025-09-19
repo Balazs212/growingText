76d18f68a4c35a2400527a4bc56416542a8e594c
szabo <szabo.balazs1214@gmail.com>
2025-09-19 09:04:53 +0200

    first

---
+++ growingText.js
@@ -0,0 +1,41 @@
+let growingText = "clean code"
+let startValue = 0
+let endValue = growingText.length
+let multiplier = 1
+let extra = 0
+
+setInterval(FullWriter, (growingText.length) * 280)
+
+function FullWriter(){
+    // for (let index = 0; index <= growingText.length; index++)
+    // {
+    //     setTimeout(() => WriteText(index), 200 * index)
+    // }
+
+    // for (let index = 0; index <= growingText.length; index++)
+    // {
+    //     setTimeout(() => WriteText(growingText.length-index), 240 * (index+growingText.length))
+    // }
+    if (multiplier==1) {
+        startValue = 0
+        endValue = growingText.length
+        extra = 0
+    }
+    else{
+        startValue = growingText.length
+        endValue = 0
+        extra = growingText.length
+    }
+    console.log(startValue)
+    for (startValue; startValue!=endValue; startValue=startValue+(1*multiplier))
+    {
+        console.log(startValue)
+        setTimeout(() => WriteText(startValue), 200 * (startValue+extra))
+    }
+    multiplier = multiplier*-1
+}
+
+function WriteText(end){
+    document.getElementById("targetLabel").innerHTML = "home of {"+growingText.substring(0,end)+"}"
+}
+
