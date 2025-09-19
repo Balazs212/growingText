d2b977406d01a20ca65d28d3650b6a3e200380c8
szabo <szabo.balazs1214@gmail.com>
2025-09-19 09:15:26 +0200

    na jo

--- growingText.js
+++ growingText.js
@@ -1,38 +1,43 @@
-let growingText = "clean code"
-let startValue = 0
-let endValue = growingText.length
-let multiplier = 1
-let extra = 0
+var growingText,
+growingText = "clean code"
 
-setInterval(FullWriter, (growingText.length) * 280)
+// let startValue = 0
+// let endValue = growingText.length
+// let multiplier = 1
+// let extra = 0
+
+setInterval(FullWriter, (growingText.length*2) * 280)
 
 function FullWriter(){
-    // for (let index = 0; index <= growingText.length; index++)
-    // {
-    //     setTimeout(() => WriteText(index), 200 * index)
-    // }
-
-    // for (let index = 0; index <= growingText.length; index++)
-    // {
-    //     setTimeout(() => WriteText(growingText.length-index), 240 * (index+growingText.length))
-    // }
-    if (multiplier==1) {
-        startValue = 0
-        endValue = growingText.length
-        extra = 0
-    }
-    else{
-        startValue = growingText.length
-        endValue = 0
-        extra = growingText.length
-    }
-    console.log(startValue)
-    for (startValue; startValue!=endValue; startValue=startValue+(1*multiplier))
+    for (let index = 0; index <= growingText.length; index++)
     {
-        console.log(startValue)
-        setTimeout(() => WriteText(startValue), 200 * (startValue+extra))
+        setTimeout(() => WriteText(index), 200 * index)
     }
-    multiplier = multiplier*-1
+
+    for (let index = 0; index <= growingText.length; index++)
+    {
+        setTimeout(() => WriteText(growingText.length-index), 240 * (index+growingText.length))
+    }
+
+    // nem sikerült 1 for ciklussal :(
+
+    // if (multiplier==1) {
+    //     startValue = 0
+    //     endValue = growingText.length
+    //     extra = 0
+    // }
+    // else{
+    //     startValue = growingText.length
+    //     endValue = 0
+    //     extra = growingText.length
+    // }
+    // console.log(startValue)
+    // for (startValue; startValue!=endValue; startValue=startValue+(1*multiplier))
+    // {
+    //     console.log(startValue)
+    //     setTimeout(() => WriteText(startValue), 200 * (startValue+extra))
+    // }
+    // multiplier = multiplier*-1
 }
 
 function WriteText(end){
